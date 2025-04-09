74b61f56b671610ce82ca65433486effdc0dbc89
khlang12 <kahyun2013@gmail.com>
2025-04-09 16:29:07 +0900

    Design: 폰트 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>

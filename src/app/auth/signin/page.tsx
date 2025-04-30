05c3f397e8661cf9443e14d7fa779773d21f6450
Hee Seo Kim <hisounni@ewhain.net>
2025-04-30 17:01:23 +0900

    로그인 페이지 style 수정

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -10,31 +10,36 @@ export default function SignInPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-md p-8 space-y-6">
-        <div className="text-center mb-12">
-          <Link href="/">
-            <button
-              onClick={scrollToTop}
-              className="text-2xl md:text-4xl tracking-tight text-gray-900 hover:text-gray-700 transition-all duration-300"
-            >
-              goggle<span className="text-sky-400">.</span>
-            </button>
-          </Link>
-        </div>
-        
-        <div className="space-y-4">
-          <GoogleLoginButton />
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-200"></div>
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-4 bg-white text-gray-500">또는</span>
-            </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex items-center justify-center">
+        <div className="w-full max-w-md p-8 space-y-6">
+          <div className="text-center mb-12">
+            <Link href="/">
+              <button
+                onClick={scrollToTop}
+                className="text-2xl md:text-4xl tracking-tight text-gray-900 hover:text-gray-700 transition-all duration-300"
+              >
+                goggle<span className="text-sky-400">.</span>
+              </button>
+            </Link>
           </div>
-          <NaverLoginButton />
+          
+          <div className="space-y-4">
+            <GoogleLoginButton />
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-200"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-4 bg-white text-gray-500">또는</span>
+              </div>
+            </div>
+            <NaverLoginButton />
+          </div>
         </div>
+      </div>
+      <div className="text-center py-4">
+        <p className="text-xs md:text-sm text-gray-500">© 2025 Goggle. All rights reserved.</p>
       </div>
     </div>
   );
